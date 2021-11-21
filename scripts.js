ee437b350d3f30238a101d4897cc9a4512b9d366
Gelo <gelo0@o2.pl>
2021-11-21 12:44:54 +0100

    Added buttons

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const sketchpad = document.querySelector("#sketchpad");
-
+const reloadBtn = document.getElementById("reload");
+const submitBtn = document.getElementById("submit");
 
 function makeRows(rows, cols) {
     sketchpad.style.setProperty('--grid-rows', rows);
@@ -8,14 +9,37 @@ function makeRows(rows, cols) {
       let cell = document.createElement("div");
       sketchpad.appendChild(cell).className = "grid-item";
     };
-  };
+};
   
-  makeRows(16, 16);
+makeRows(16, 16);
+draw();
 
-const gridItems = document.querySelectorAll(".grid-item")  
+let rows = 0;
+let cols = 0;
 
-gridItems.forEach((div) => {
-    div.addEventListener('mouseenter', () => {
-      div.style.cssText = "background-color: black";
+reloadBtn.addEventListener('click', () => {
+    while (sketchpad.firstChild){
+        sketchpad.removeChild(sketchpad.firstChild);
+    }
+    makeRows(16, 16);
+    draw();
+});
+
+function draw(){
+    const gridItems = document.querySelectorAll(".grid-item") 
+    gridItems.forEach((div) => {
+        div.addEventListener('mouseenter', () => {
+          div.style.cssText = "background-color: black";
+        });
     });
-  });
+}
+
+submitBtn.addEventListener('click', () =>{
+    let text=document.getElementById('grid-size').value;
+    console.log(text);
+    while (sketchpad.firstChild){
+        sketchpad.removeChild(sketchpad.firstChild);
+    }
+    makeRows(text, text);
+    draw();
+});
